6ee5a5ac548ece3ec5601257a110834e17c638d5
admin <admin@admins-MacBook-Air.local>
2021-04-11 23:15:11 +0530

    ADDED: employee is present or absent

---
+++ EmployeeWage.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+
+
+
+let isFullTime = 1
+
+print("Welcome to Employee Wage")
+
+var empCheck = (arc4random() % 2)
+
+if empCheck == isFullTime
+
+{
+
+    print("Employee Is Present")
+
+}else{
+
+    print("Employee Is Absent")
+
+}
+
