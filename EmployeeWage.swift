bc0b28b8e4abaf7384e554234a48166b7fb75e4b
admin <admin@admins-MacBook-Air.local>
2021-04-11 23:49:20 +0530

    ADDED:  calculate employee wage till working day or working hrs reahed

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -6,10 +6,9 @@ let isPartTime = 2
 
 let empRatePerHrs = 20
 
-let NumOfWorkingDays = 20
+let numOfWorkingDays = 20
 
-
-
+let maxHrsInMonth = 100
 
 print("Welcome to Employee Wage")
 
@@ -19,34 +18,49 @@ var empWage = 0
 
 var totalEmpWage = 0
 
-for _ in 0..< NumOfWorkingDays{
+var totalEmpHrs = 0
+
+var totalWorkingDays = 0
+
+while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays )
+
+{
+
+    totalWorkingDays += 1
+
     let empCheck = (Int)(arc4random() % 3)
-switch empCheck{
 
-    case isFullTime :
+    switch empCheck{
 
-        empHrs = 8
+        case isFullTime :
+
+            empHrs = 8
 
     
 
-    case isPartTime :
+        case isPartTime :
 
-        empHrs = 4
+            empHrs = 4
 
-    default:
+    
 
-        empHrs = 0
+        default:
+
+            empHrs = 0
 
 }
 
-    
-empWage = empHrs * empRatePerHrs
+    totalEmpHrs += empHrs
 
-totalEmpWage += empWage
-    
+    print("Day: \(totalWorkingDays)")
+
+    print("Emp Hrs: \(empHrs) ")
+
+  
 }
 
-print("Employee Wage: \(empWage)")
+totalEmpWage = totalEmpHrs * empRatePerHrs
 
 print("Total Employee wage :\(totalEmpWage)")
+
 
