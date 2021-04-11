79277e05045c7ab4ceba57830ab4aaec5aba6582
admin <admin@admins-MacBook-Air.local>
2021-04-12 00:11:17 +0530

    ADDED:  calculate employee wage for multiple company

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -4,14 +4,29 @@ class Employee_Wage{
 
 let isFullTime = 1
 
-let isPartTime = 2
+let isParttime = 2
+    
+let companyName:String
 
-let empRatePerHrs = 20
+let empRatePerHrs:Int
 
-let numOfWorkingDays = 20
+let numOfWorkingDays:Int
 
-let maxHrsInMonth = 100
+let maxHrsInMonth:Int
 
+    init(companyName:String, empRatePerHrs:Int, numOfWorkingDays:Int, maxHrsInMonth:Int) {
+
+        self.companyName = companyName
+
+        self.empRatePerHrs = empRatePerHrs
+
+        self.numOfWorkingDays = numOfWorkingDays
+
+        self.maxHrsInMonth = maxHrsInMonth
+
+    }
+
+    
 
 }
 
@@ -27,6 +42,7 @@ func wageCalculation(emp:Employee_Wage){
 
     var totalWorkingDays = 0
 
+
     while (totalEmpHrs <= emp.maxHrsInMonth && totalWorkingDays < emp.numOfWorkingDays)
 
     {
@@ -41,28 +57,50 @@ func wageCalculation(emp:Employee_Wage){
 
                 empHrs = 8
 
+    
+
             case emp.isPartTime:
 
                 empHrs = 4
 
-   
-       	   default:
+    
+
+            default:
 
                 empHrs = 0
 
         }
-        totalEmpHrs += empHrs
+
+
+       totalEmpHrs += empHrs
 
         print("Day: \(totalWorkingDays)")
 
         print("Emp Hrs: \(empHrs) ")
+
+   
     }
+
+    print("-----------------Company Name :\(emp.companyName)——————————“)
 
     totalEmpWage = totalEmpHrs * emp.empRatePerHrs
 
     print("Total Employee wage :\(totalEmpWage)")
+
+    
+
 }
 
+var company1 = Employee_Wage(companyName: "Reliance", empRatePerHrs: 20, numOfWorkingDays: 25, maxHrsInMonth: 20)
 
-var employee = Employee_Wage()
-wageCalculation(emp: employee)
+wageCalculation(emp: company1)
+
+var company2 = Employee_Wage(companyName: "HDFC", empRatePerHrs: 25, numOfWorkingDays: 28, maxHrsInMonth: 45)
+
+wageCalculation(emp: company2)
+
+var company3 = Employee_Wage(companyName: "Jindal", empRatePerHrs: 29, numOfWorkingDays: 18, maxHrsInMonth: 70)
+
+wageCalculation(emp: company3)
+
+
